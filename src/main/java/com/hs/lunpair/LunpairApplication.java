e51816bac4a78a048a604d3c05b282680490573a
gnsta <gilmat@naver.com>
2020-10-11 16:40:46 +0900

    초기 설정

---
+++ src/main/java/com/hs/lunpair/LunpairApplication.java
@@ -0,0 +1,13 @@
+package com.hs.lunpair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunpairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunpairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hs/lunpair/LunpairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.lunpair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunpairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
